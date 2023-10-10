c3ca25e87fa77cab132bed5ce6f3ef905d465542
01435435 <dukewen@sf-express.com>
2023-10-10 15:27:42 +0800

    update 1-20

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32) -> i32{
+    let price = if(num<=40){2}else{1};
+    num*price
+}
 
 // Don't modify this function!
 #[test]
