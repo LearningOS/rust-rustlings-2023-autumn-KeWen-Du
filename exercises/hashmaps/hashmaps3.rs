335f12b1f0e8048bb34c3ef33b5e81b4c6e2d1bc
01435435 <dukewen@sf-express.com>
2023-10-19 20:03:52 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let g = scores.entry(team_1_name.to_string()).or_insert(Team{goals_scored:0,goals_conceded:0});
+        (*g).goals_scored = (*g).goals_scored + team_1_score;
+        (*g).goals_conceded = (*g).goals_conceded + team_2_score;
+        let f = scores.entry(team_2_name.to_string()).or_insert(Team{goals_scored:0,goals_conceded:0});
+        (*f).goals_scored = (*f).goals_scored + team_2_score;
+        (*f).goals_conceded = (*f).goals_conceded + team_1_score;
     }
     scores
 }
